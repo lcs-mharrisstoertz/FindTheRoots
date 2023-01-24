fb0c0ab6fb64d1a832b4a0c93490de7bacc19a17
Morgan <Mharrisstoertz@gmail.com>
2023-01-24 11:37:47 -0500

    Initial Commit

---
+++ FindTheRoots/FindTheRootsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindTheRootsApp.swift
+//  FindTheRoots
+//
+//  Created by Morgan Harris-Stoertz on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct FindTheRootsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
